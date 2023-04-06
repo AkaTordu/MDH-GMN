f070e612e83ef33f6ab5d81210ff4aefc9952149
AkaTordu <maximilienlandes@gmail.com>
2023-04-06 16:28:01 +0200

    Load prodcuts from categories

--- app/src/main/java/com/epsi/mdhgmn/ProdcutAdapter.kt
+++ app/src/main/java/com/epsi/mdhgmn/ProdcutAdapter.kt
@@ -5,36 +5,37 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.epsi.mdhgmn.CategoryItem
+import com.epsi.mdhgmn.ProductItem
 import com.epsi.mdhgmn.ProductsActivity
 import com.epsi.mdhgmn.R
 
-class CategoryAdapter(private val categoryItems: List<CategoryItem>) :
-    RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+class ProductAdapter(private val productItems: List<ProductItem>) :
+    RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val categoryButton: Button = itemView.findViewById(R.id.categoryButton)
+        val productButton: Button = itemView.findViewById(R.id.productButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_category, parent, false)
+            .inflate(R.layout.item_product, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val categoryItem = categoryItems[position]
-        holder.categoryButton.text = categoryItem.title
+        val productItem = productItems[position]
+        holder.productButton.text = productItem.name
 
-        holder.categoryButton.setOnClickListener {
+        holder.productButton.setOnClickListener {
             val context = holder.itemView.context
             val intent = Intent(context, ProductsActivity::class.java)
-            intent.putExtra("products_url", categoryItem.products_url)
-            intent.putExtra("title", categoryItem.title)
+            intent.putExtra("picture_url", productItem.picture_url)
+            intent.putExtra("name", productItem.name)
             context.startActivity(intent)
         }
     }
 
     override fun getItemCount(): Int {
-        return categoryItems.size
+        return productItems.size
     }
 }
